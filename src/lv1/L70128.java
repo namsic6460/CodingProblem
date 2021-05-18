b0ecc83bb865c5ac4e34d9b0c0e119014b2e8bd9
user <namsic6460@gmail.com>
2021-05-18 13:35:58 +0900

    Create L70128.java

---
+++ src/lv1/L70128.java
@@ -0,0 +1,19 @@
+package lv1;
+
+public class L70128 {
+
+	public static void main(String[] args) {
+		new L70128().solution(new int[] {1, 2, 3, 4}, new int[] {-3, -1, 0, 2});
+	}
+
+	public int solution(int[] a, int[] b) {
+		int answer = 0;
+		
+		for(int i = 0; i < a.length; i++) {
+			answer += a[i] * b[i];
+		}
+		
+		return answer;
+	}
+
+}
